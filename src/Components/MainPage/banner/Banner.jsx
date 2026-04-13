756506bea45fea967750d56ebb09509abeb81a10
topu9872-cpu <topu9872@gmail.com>
2026-04-14 03:59:15 +0600

    i have completed banner

--- src/Components/MainPage/banner/Banner.jsx
+++ src/Components/MainPage/banner/Banner.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
 
 const Banner = () => {
     return (
-        <div>
-            banner
-        </div>
+        <div className="hero bg-base-200 min-h-screen text-center ">
+  <div className="hero-content ">
+    <div className="w-full ">
+      <h1 className="text-5xl lg:text-nowrap font-bold">Friends to keep close in your life</h1>
+      <p className="py-6">
+       Your personal shelf of meningful connections, Browse, tend and nurture the <br/> relationships that matter nost.
+       
+      </p>
+      <button className="btn btn-primary"><FiPlus/> Add a Friend</button>
+    </div>
+  </div>
+</div>
+        
+        
     );
 };
 
